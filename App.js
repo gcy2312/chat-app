700224a782b4364ff3bfe1b859a2934d964edb56
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-07 11:17:41 -0400

    add logbox to remove warnings (show only console)

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 //import components
@@ -16,6 +17,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Create the navigator
 const Stack = createStackNavigator();
 
+// Ignore error and warning alerts inside the app (show only in the console).
+LogBox.ignoreAllLogs();
+
 export default function App() {
   return (
     <NavigationContainer>
@@ -29,6 +33,7 @@ export default function App() {
         <Stack.Screen
           name="Chat"
           component={Chat}
+
         />
       </Stack.Navigator>
     </NavigationContainer>
